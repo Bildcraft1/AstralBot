6534a070abcfc8ed05074143c336fa31f7d06385
italiano at gentoo <bildcraft10000@gmail.com>
2020-07-30 23:57:52 +0200

    Update AstralBot.js

--- AstralBot.js
+++ AstralBot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '??';
 const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+const guildInvites = new Map();
 
 client.on('ready', () => {
     console.log('========AstralNetwork Bot========');
@@ -171,6 +172,36 @@ client.on('messageDelete', message => {
                 .setTimestamp();
             channel.send(embed);
         }
+    }
+});
+
+client.on('inviteCreate', async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchInvites()));
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in.`);
+    client.guilds.cache.forEach(guild => {
+        guild.fetchInvites()
+            .then(invites => guildInvites.set(guild.id, invites))
+            .catch(err => console.log(err));
+    });
+});
+
+client.on('guildMemberAdd', async member => {
+    const cachedInvites = guildInvites.get(member.guild.id);
+    const newInvites = await member.guild.fetchInvites();
+    guildInvites.set(member.guild.id, newInvites);
+    try {
+        const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses);
+        const embed = new Discord.MessageEmbed()
+            .setDescription(`${member.user.tag} is the ${member.guild.memberCount} to join.\nJoined using ${usedInvite.inviter.tag}\nNumber of uses: ${usedInvite.uses}`)
+            .setTimestamp()
+            .setTitle(`${usedInvite.url}`);
+        const welcomeChannel = member.guild.channels.cache.find(channel => channel.id === '640340055201939456');
+        if(welcomeChannel) {
+            welcomeChannel.send(embed).catch(err => console.log(err));
+        }
+    }
+    catch(err) {
+        console.log(err);
     }
 });
 
