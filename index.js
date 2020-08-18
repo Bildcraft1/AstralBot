bf6cbb25cbf7cfe291082608f339f327cd2f61bc
Bildcraft1 <bildcraft10000@gmail.com>
2020-08-18 09:32:55 +0200

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const guildInvites = new Map();
+const Enmap = require("enmap");
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
