a6de8d2365a2bb84534060abf9fd7fee44b037f0
Bildcraft1 <bildcraft10000@gmail.com>
2020-08-18 11:43:04 +0200

    Added log messages

--- cmds/ban.js
+++ cmds/ban.js
@@ -1,13 +1,14 @@
 exports.run = (client, message, [mention, ...reason]) => {
     const modRole = message.guild.roles.cache.find(role => role.name === process.env.STAFF_ROLE);
     if (!modRole)
+        return message.reply("The Mods role does not exist");
         return console.log("The Mods role does not exist");
 
     if (!message.member.roles.cache.has(modRole.id))
         return message.reply("You can't use this command.");
 
     if (message.mentions.members.size === 0)
-        return message.reply("Please mention a user to banned");
+        return message.reply("Please mention a user to ban");
 
     if (!message.guild.me.hasPermission("BAN_MEMBERS"))
         return message.reply("");
